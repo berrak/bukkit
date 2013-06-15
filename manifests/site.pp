1634ad5771415a056606acd657b22f2571722386
berrak <bkronmailbox-git@yahoo.se>
2013-06-15 20:17:33 +0200

    Set up roots directories

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,9 @@ node 'kronlund01.levonline.com' {
     
     include puppetize
         
-    class { le_hosts::config :
-         puppetserver_hostname => 'kronlund01'
-    }
+    class { le_hosts::config : puppetserver_hostname => 'kronlund01' }
+    
+    include le_root_home
+    
     
 }
