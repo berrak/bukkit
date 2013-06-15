04c25e55af4c15f467968caa7e86d66560ddce8c
berrak <bkronmailbox-git@yahoo.se>
2013-06-15 20:21:34 +0200

    Removed duplicate declaration of directory creation

--- modules/le_root_home/manifests/init.pp
+++ modules/le_root_home/manifests/init.pp
@@ -17,14 +17,6 @@ class le_root_home {
 		subscribe => File["/root"],
 		refreshonly => true,
 	}
-
-	# create a bin subdirectory directory only for root's binary tools
-	file { "/root/bin":
-		ensure => "directory",
-		owner => 'root',
-		group => 'root',
-		mode => '0700',
-	}
 	
 	# create a jobs subdirectory directory only for local admins cron scripts
 	file { "/root/jobs":
