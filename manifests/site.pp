e5b7af25301ceba937a2063aee38532aff2c0fed
berrak <bkronmailbox-git@yahoo.se>
2013-06-16 09:59:20 +0200

    Removed iptables manifests for fault finding on target node

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node 'kronlund01.levonline.com' {
         
     class { le_hosts::config : puppetserver_hostname => 'kronlund01' }
     
-    class { le_iptables::config : puppetserver_hostname => 'kronlund01' }
+    # class { le_iptables::config : puppetserver_hostname => 'kronlund01' }
     
 
 }
--- modules/le_iptables/modules/init.pp
+++
@@ -1,8 +0,0 @@
-##
-## This class manage iptables
-##
-class le_iptables {
-
-	include le_iptables::install, le_iptables::config
-	
-}
