d9fe1ab58349449fc279148406a371a4ffa20ca9
berrak <bkronmailbox-git@yahoo.se>
2013-12-19 17:52:05 +0100

    Set up http and postgresql

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,10 +10,36 @@ node 'kronlund01.levonline.com' {
     class { le_hosts::config : puppetserver_hostname => 'kronlund01' }
     class { le_iptables::config : puppetserver_hostname => 'kronlund01' }
     
-    # modules from virtualhost repo
     
     
+    ## modules from virtualhost repo ##
+    
+    include vb_root_bashrc
+    
+    # configure APT
+    include vb_aptconf
+    
+    # use apache2 prefork
+    include vb_apache2
+    
+    # Define a new Apache2 virtual host (suexec to user jensen)    
+    vb_apache2::vhost { 'jensen.mc-butter.se' :
+            priority => '001',
+          devgroupid => 'jensen',
+          execscript => 'suexec',
+    }        
+    
+    # user profile
+    vb_user_bashrc::config { 'jensen' : }
+    
+    # postgreSQL-9.1
+    include vb_postgresql
+    
+    # packages without any special configurations
+    class { vb_install_debs : debs => [ "build-essential", "tree" ] }
+    
+    # partial install of required debian pacakges for OC-ESQL pre-compiler
+    include vb_ocesql
     
     
-
 }
