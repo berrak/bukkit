94ddabbd94d070e1211e045fa2a1f94102fc5a2a
berrak <bkronmailbox-git@yahoo.se>
2013-12-19 22:35:37 +0100

    Add opencobol compiler

--- modules/vb_ocesql/manifests/install.pp
+++ modules/vb_ocesql/manifests/install.pp
@@ -3,14 +3,14 @@
 #
 class vb_ocesql::install {
 
-    # Other Debian standard tools should already be installed
+    # need the opencobol complier
+    package { "open-cobol"   : ensure => installed }
 
     # yacc och flex compatible tools
     package { "bison"        : ensure => installed }
     package { "flex"         : ensure => installed }
 
-    
-    # standard autotools
+        # standard autotools
     package { "automake"     : ensure => installed }
     package { "autoconf"     : ensure => installed }
     package { "libtool"      : ensure => installed }
