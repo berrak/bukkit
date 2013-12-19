831ccdb9512440ebe307a5307a7e9b784a9f764f
berrak <bkronmailbox-git@yahoo.se>
2013-12-19 19:44:11 +0100

    Test postgresql connection

--- modules/vb_postgresql/manifests/config.pp
+++ modules/vb_postgresql/manifests/config.pp
@@ -14,5 +14,15 @@ class vb_postgresql::config {
         require => Class["vb_postgresql::install"],
         notify => Service["postgresql"],
     }
+	
+    file { '/etc/postgresql/9.1/main/postgresql.conf':
+         source => "puppet:///modules/vb_postgresql/postgresql.conf",    
+          owner => 'postgres',
+          group => 'postgres',
+		  mode  => '0644',
+        require => Class["vb_postgresql::install"],
+        notify => Service["postgresql"],
+    }	
+	
     
 }
