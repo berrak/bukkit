5cca502552d41e840b10c511aade2e4cb910cc2a
berrak <bkronmailbox-git@yahoo.se>
2013-12-19 16:55:15 +0100

    Add rule for port 80

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,14 +4,16 @@
 node 'kronlund01.levonline.com' {
     
     include puppetize
-    
     include le_root_home
-    
     include le_screen
         
     class { le_hosts::config : puppetserver_hostname => 'kronlund01' }
-    
     class { le_iptables::config : puppetserver_hostname => 'kronlund01' }
+    
+    # modules from virtualhost repo
+    
+    
+    
     
 
 }
