1e8eceafe895c2e95f9715b1e7f49df35d3ddb86
berrak <bkronmailbox-git@yahoo.se>
2014-01-10 15:01:45 +0100

    Remove old fail2ban module

--- modules/le_fail2ban/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-##
-## Manage fail2ban
-##
-class le_fail2ban {
-
-    include le_fail2ban::install, le_fail2ban::config, le_fail2ban::service
-
-}
--- modules/le_fail2ban/manifests/install.pp
+++
@@ -1,8 +0,0 @@
-##
-## Install fail2ban
-##
-class le_fail2ban::install {
-
-    package { "fail2ban": ensure => installed }
-    
-}
