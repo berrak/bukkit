5c8e12d4df6cdcceab4e5a051cf9b5f0d74c366e
berrak <bkronmailbox-git@yahoo.se>
2014-01-10 21:23:27 +0100

    Script name must be unique

--- modules/le_build/manifests/make.pp
+++ modules/le_build/manifests/make.pp
@@ -50,8 +50,8 @@ define le_build::make ( $projectname='', $username='', $groupname='') {
     
 	# install the remote helper script
 	
-	file { "/home/${username}/bin/$remote_install_scriptname":
-		content =>  template("le_build/$remote_install_scriptname.erb"),  
+	file { "/home/${username}/bin/${username}_${remote_install_scriptname}":
+		content =>  template("le_build/${username}_${remote_install_scriptname}.erb"),  
 		  owner => $username,
 		  group => $groupname,
 		   mode => '0755',
