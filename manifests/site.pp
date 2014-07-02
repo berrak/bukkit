c2478ffa620bf0079ddd706dd9d32763e5dec02e
berrak <bkronmailbox-git@yahoo.se>
2014-07-02 19:24:45 +0200

    Prepare domain name change

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,12 @@
 #
 # site.pp
 #
-node 'www-node.mc-butter.se' {
+node 'www-node.debinix.org' {
     
     include puppetize
     
+    ### modules from bukkit repo ###	
+	
     include le_root_home
     include le_screen
 	include le_logwatch
@@ -17,12 +19,11 @@ node 'www-node.mc-butter.se' {
 	# installs iptables + fail2ban
     class { le_iptables::config : puppetserver_hostname => 'www' }
     
-    ## modules from virtualhost repo ##
+	
+	
+    ### modules from virtualhost repo ###
     
     include vb_puppet_utils
-    
-    # packages without any special configurations (assumes php5 is installed by ISP)
-    class { vb_install_debs : debs => [ "build-essential", "tree", "dnsutils", "locales-all", "php5-curl" ] }    
     
     include vb_root_bashrc
     
@@ -31,64 +32,15 @@ node 'www-node.mc-butter.se' {
     
     # use apache2 prefork
     include vb_apache2 
-    
-    # Define a new Apache2 virtual host (suexec to user jensen)    
-    vb_apache2::vhost { 'www.mc-butter.se' :
+    	
+	## Define a new Apache2 virtual host (docroot directory writable by group 'bekr')
+    vb_apache2::vhost { 'www.debinix.org' :
             priority => '001',
-          devgroupid => 'jensen',
-         execscript => 'suexec',
-    }        
-    
-    # user profile
-    vb_user_bashrc::config { 'jensen' : }
-    
-    # postgreSQL-9.1
-    include vb_postgresql
-    
-	# Create database for the application and owner (option: 2nd databaseuser)
-    vb_postgresql::create_database { 'openjensen' :
-		databaseowner => 'jensen',
-		databaseuser => '' }	    
-    
-    # partial install for OC-ESQL pre-compiler. The source 
-    # OSS Consortium archieve have to be installed manually
-    include vb_ocesql
-    
-    
-    ## set up project structure (no spaces in names) and makefiles
-    
-    class { le_build::project :
-      projectname => 'openjensen',
-		 username => 'jensen',
-        groupname => 'jensen',
-    }
-    
-    le_build::make { 'src' :
-     	projectname => 'openjensen',
-		   username => 'jensen',
-          groupname => 'jensen',
-    }
-    
-    le_build::make { 'html' :
-     	projectname => 'openjensen',
-		   username => 'jensen',
-          groupname => 'jensen',
-    }
-    
-    le_build::make { 'lib' :
-     	projectname => 'openjensen',
-		   username => 'jensen',
-          groupname => 'jensen',
+          devgroupid => 'bekr',
+          execscript => 'none',
     }
 	
-    le_build::make { 'php' :
-     	projectname => 'openjensen',
-		   username => 'jensen',
-          groupname => 'jensen',
-    }    	
-    
-    # Ensure daily cron backup
-    
-    le_build::backup { 'jensen' : projectname => 'openjensen' }     
+    # user profile
+    vb_user_bashrc::config { 'bekr' : }
     
 }
