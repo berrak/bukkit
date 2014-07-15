de56614764d06a2d69750932096f5d5eff8e652e
berrak <bkronmailbox-git@yahoo.se>
2014-07-15 21:19:44 +0200

    Fix typ2

--- modules/le_sudo/manifests/config.pp
+++ modules/le_sudo/manifests/config.pp
@@ -6,7 +6,7 @@ define le_sudo::config {
 
     include le_sudo
 
-    exec { "Add_{$name}_To_Administrator_Group" :
+    exec { "Add_${name}_To_Administrator_Group" :
             command => "usermod -a -G sudo $name",
                path => '/usr/bin:/usr/sbin:/bin',
              unless => "groups $name | grep sudo",
